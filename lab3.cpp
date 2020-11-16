15480c570e9952905aa79d9ab9be425fec46a0fe
szymonobara <Szymon2904@gmail.com>
2020-11-16 18:02:15 +0000

    solved

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
 
+class BytGeometryczny
+{
+public:
+    virtual void id() = 0;
+};
+
+class Figura : public BytGeometryczny
+{
+public:
+    Figura(double pole_) : pole{pole_} {}
+    virtual ~Figura() { std::cout << "Usunieto Figura \n"; }
+    double getPole() { return pole; }
+    // virtual void id() { std::cout << "Figura o polu: " << getPole() << "\n"; }
+
+private:
+    double pole;
+};
+
+class Kwadrat : public Figura
+{
+public:
+    Kwadrat(double bok) : Figura{bok * bok} {}
+    virtual ~Kwadrat() { std::cout << "Usunieto Kwadrat \n"; }
+    void id() override { std::cout << "Kwadrat o polu: " << getPole() << "\n"; }
+};
+
+class Kolo : public Figura
+{
+public:
+    Kolo(double promien) : Figura{3.14 * promien * promien} {}
+    virtual ~Kolo() { std::cout << "Usunieto Kolo \n"; }
+    void id() override { std::cout << "Kolo o polu: " << getPole() << "\n"; }
+};
+
+void idWolno(Figura& figura)
+{
+    figura.id();
+}
+
 int main()
 {
-    puts("Hello, World!");
+    BytGeometryczny* bg = new Kwadrat{5};
+    bg->id();
 }
--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,17 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+  int count = 0;
+	if (towar == 0) return 0;
+	while (1) {
+		Stocznia stocznia{};
+		Statek* s1 = stocznia();
+		s1->transportuj();
+		Zaglowiec* t = dynamic_cast<Zaglowiec*>(s1);
+		if (t != 0) { count++; }
+		delete s1;
+		if (towar <= stocznia.getTotalCap()) { break; }
+
+	}
+	return count;
 }
